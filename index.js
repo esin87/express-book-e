cbd30318edb1ac4844455e36efa0f7912b42c737
esin87 <esin.saribudak@gmail.com>
2022-06-30 13:12:16 -0500

    Set up basic app configuration

---
+++ index.js
@@ -0,0 +1,37 @@
+//=============================================================================
+// Basic Config
+//=============================================================================
+const express = require('express');
+// instantiate express
+const app = express();
+// in deployment, port will be set for us with process.env.PORT
+// in development, app will run on localhost:8000
+app.set('port', process.env.PORT || 8000);
+
+//=============================================================================
+// Middleware
+//=============================================================================
+// Middleware -- functions that run before processing a request and sending a response
+// `express.json` parses application/json request data and
+//  adds it to the request object as request.body
+app.use(express.json());
+// `express.urlencoded` parses x-ww-form-urlencoded request data and
+//  adds it to the request object as request.body
+app.use(express.urlencoded({ extended: true }));
+
+//=============================================================================
+// ROUTES
+//=============================================================================
+app.get('/', (req, res) => {
+	res.redirect('/api/bookmarks');
+});
+
+/* START CONTROLLERS HERE */
+/* END CONTROLLERS HERE */
+
+//=============================================================================
+// START SERVER
+//=============================================================================
+app.listen(app.get('port'), () => {
+	console.log(`✅ PORT: ${app.get('port')} 🌟`);
+});
